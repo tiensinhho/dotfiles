b7b279313aac5aae20c55935c8d88910f8a749e7
Sinh Ho <147224855+tiensinhho@users.noreply.github.com>
2026-08-16 13:41:07 +0700

    Create local bin directory for Starship installation
    Ensure the local bin directory is created before installation.

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ echo "===== START ====="
 
 if [ "$(uname -s)" = "Linux" ]; then
     if ! command -v starship >/dev/null 2>&1; then
+        mkdir -p "$HOME/.local/bin"
         curl -sS https://starship.rs/install.sh | sh -s -- --yes --bin-dir "$HOME/.local/bin"
     fi
 fi
