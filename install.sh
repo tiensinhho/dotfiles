d3b6072adfd77c95e47928b984a561ccfb243c53
Sinh Ho <147224855+tiensinhho@users.noreply.github.com>
2026-08-16 13:38:26 +0700

    Modify starship install command and clean up script
    Update starship installation to specify binary directory and remove unnecessary comments.

--- install.sh
+++ install.sh
@@ -3,12 +3,9 @@ echo "===== START ====="
 
 if [ "$(uname -s)" = "Linux" ]; then
     if ! command -v starship >/dev/null 2>&1; then
-        curl -sS https://starship.rs/install.sh | sh -s -- --yes
+        curl -sS https://starship.rs/install.sh | sh -s -- --yes --bin-dir "$HOME/.local/bin"
     fi
 fi
-
-# 5. Tạo liên kết file cấu hình (Dùng đường dẫn tuyệt đối $HOME cho an toàn)
-echo "Đang tạo liên kết file cấu hình..."
 
 ln -sf "$HOME/dotfiles/.bashrc" "$HOME/.bashrc"
 
