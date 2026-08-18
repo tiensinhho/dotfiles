49a0e630306020d209e6a7a324d1584d707bd319
Sinh Ho <147224855+tiensinhho@users.noreply.github.com>
2026-08-18 13:57:06 +0700

    Remove starship installation from install.sh
    Removed installation of starship and related checks.

--- install.sh
+++ install.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 echo "===== START ====="
-
-if [ "$(uname -s)" = "Linux" ]; then
-    if ! command -v starship >/dev/null 2>&1; then
-        mkdir -p "$HOME/.local/bin"
-        curl -sS https://starship.rs/install.sh | sh -s -- --yes --bin-dir "$HOME/.local/bin"
-    fi
-fi
-
 ln -sf "$HOME/dotfiles/.bashrc" "$HOME/.bashrc"
-mkdir -p "$HOME/.config"
-ln -sf "$HOME/dotfiles/starship.toml" "$HOME/.config/starship.toml"
 
 echo "===== END ====="
