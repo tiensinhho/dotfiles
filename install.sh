5d2fb4e9d31307f22dc761b5d885fc8c1c53f1cf
Sinh Ho <147224855+tiensinhho@users.noreply.github.com>
2026-08-30 15:30:42 +0700

    update

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 echo "===== START ====="
 
-if [ "$(uname -s)" = "Linux" ]; then
-    if ! command -v starship >/dev/null 2>&1; then
-        mkdir -p "$HOME/.local/bin"
-        curl -sS https://starship.rs/install.sh | sh -s -- --yes --bin-dir "$HOME/.local/bin"
-    fi
-fi
+# if [ "$(uname -s)" = "Linux" ]; then
+#     if ! command -v starship >/dev/null 2>&1; then
+#         mkdir -p "$HOME/.local/bin"
+#         curl -sS https://starship.rs/install.sh | sh -s -- --yes --bin-dir "$HOME/.local/bin"
+#     fi
+# fi
 
 ln -sf "$HOME/dotfiles/.bashrc" "$HOME/.bashrc"
-ln -sf "$HOME/dotfiles/.config/starship.toml" "$HOME/.config/starship.toml"
+# ln -sf "$HOME/dotfiles/.config/starship.toml" "$HOME/.config/starship.toml"
 
 echo "===== END ====="
